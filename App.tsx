178932f29a8ae1e70ac2eebda2c074175a06181d
CrowsVeldt <zechy.mahler@gmail.com>
2024-03-24 09:23:54 -0600

    feat: add drawerMenu component, update app stack code

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -17,23 +18,19 @@ export default function App() {
       <NavigationContainer>
         <GestureHandlerRootView style={{ flex: 1 }}>
           <DateProvider>
-            <Stack.Navigator>
-              <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{
-                  title: "Home",
-                  headerStyle: { backgroundColor: "#00152d" },
-                  headerTitleStyle: { color: "white" },
-                }}
-              />
+            <Stack.Navigator
+              initialRouteName="Girl Genius"
+              screenOptions={{
+                headerStyle: { backgroundColor: "#00152d" },
+                headerTitleStyle: { color: "white" },
+              }}
+            >
+              <Stack.Screen name="Girl Genius" component={HomeScreen} />
               <Stack.Screen
                 name="Bookmarks"
                 component={BookmarkScreen}
                 options={{
                   title: "Bookmarks",
-                  headerStyle: { backgroundColor: "#00152d" },
-                  headerTitleStyle: { color: "white" },
                 }}
               />
               <Stack.Screen
@@ -41,8 +38,6 @@ export default function App() {
                 component={ListScreen}
                 options={{
                   title: "List",
-                  headerStyle: { backgroundColor: "#00152d" },
-                  headerTitleStyle: { color: "white" },
                 }}
               />
             </Stack.Navigator>
