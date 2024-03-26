15749a374a2cf1c1adbbe59a15f867bc6a220c94
CrowsVeldt <zechy.mahler@gmail.com>
2024-03-26 11:06:21 -0600

    feat: add flatlist and renderItem to volumelist

--- components/nav table components/VolumeList.tsx
+++ components/nav table components/VolumeList.tsx
@@ -1,8 +1,9 @@
 import { Text, View } from "react-native";
 import { volumeObject } from "../../utils/types";
+import { useState } from "react";
+import { FlatList } from "react-native-gesture-handler";
 
-// make table with act, volume, title, date
-// start with table collapsed, open on click
+const renderItem = (data: string) => <Text>{data}</Text>;
 
 export const VolumeList = ({
   currentVolume,
@@ -11,14 +12,25 @@ export const VolumeList = ({
   currentVolume: volumeObject;
   index: number;
 }) => {
+  const [open, setOpen] = useState(false);
   const { volume, titles, dates } = currentVolume;
 
-    // add titles and dates under volume header
-    // align titles with the date they share
+    // for each date find if a title matches it
+    // if yes, return item = [date, title]
+    // else return item = [date, null]
+
+    // if item[1] != null, render 'date-title'
+    // else render 'date'
 
   return (
     <View>
       <Text>{`Volume ${index}`}</Text>
+      <FlatList
+        data={dates}
+        renderItem={({ item, index, separators }) =>
+          open ? renderItem(item) : null
+        }
+      />
     </View>
   );
 };
