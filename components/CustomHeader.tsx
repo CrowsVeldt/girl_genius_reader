6268eaee5248ccf9222d1e0ca800ff03656aacf1
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-03 08:02:06 -0600

    ui: improve menu button styles

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,4 +1,11 @@
-import { View, Text, Button, StyleSheet, StatusBar } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 import StarButton from "./StarButton";
 import { useContext } from "react";
 import { ComicContext } from "../context/ComicContext";
@@ -16,16 +23,21 @@ export default function CustomHeader({
 }) {
   const { getCurrentDate } = useContext(ComicContext);
 
-  const date = getCurrentDate()
+  const date = getCurrentDate();
   const title = route.name;
 
-    // Get Volume# and Page# from ComicContext
+  // Get Volume# and Page# from ComicContext
 
   return (
     <View style={[options.headerStyle, styles.header]}>
-      <Button title={"≡"} onPress={() => navigation.openDrawer()} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.openDrawer()}
+      >
+        <Text style={styles.buttonText}>≡</Text>
+      </TouchableOpacity>
       <Text>{title}</Text>
-          <StarButton date={date} />
+      <StarButton date={date} />
     </View>
   );
 }
@@ -33,7 +45,16 @@ export default function CustomHeader({
 const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
-        justifyContent: "space-between",
+    justifyContent: "space-between",
     marginTop: StatusBar.currentHeight,
+  },
+  button: {
+    backgroundColor: process.env.EXPO_PUBLIC_DARK_BG_COLOR,
+    width: 50,
+    alignItems:'center',
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 30,
   },
 });
