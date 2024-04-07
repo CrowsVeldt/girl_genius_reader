2734794e68437af02f39881269539c8ce5341802
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-06 20:46:15 -0600

    test: add test file for storage.ts

---
+++ __tests__/utils/storage.test.ts
@@ -0,0 +1,6 @@
+import { saveData, retrieveData } from "../../src/utils/storage";
+
+// https://dev.to/tiaeastwood/how-to-mock-and-test-asyncstorage-in-react-native-4kil
+
+describe("saveData", () => {});
+describe("retrieveData", () => {});
