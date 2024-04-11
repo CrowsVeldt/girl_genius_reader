b7db9d6c1eaaf32818e6ff7b6dfb4b21cc35122a
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-11 08:49:29 -0600

    dev: add snapshot test for comiclink

---
+++ __tests__/src/components/ComicLink.test.tsx
@@ -0,0 +1,21 @@
+import renderer from "react-test-renderer";
+import ComicLink from "../../../src/components/ComicLink";
+import ComicProvider from "../../../src/context/ComicContext";
+
+describe("<ComicLink />", () => {
+  const testPage = {
+    date: "20021104",
+    title: "",
+    pageNumber: 1,
+    volume: 1,
+  };
+
+  it("renders the same as last time", () => {
+    const tree = renderer.create(
+      <ComicProvider>
+        <ComicLink page={testPage} nav={jest.mock} />
+      </ComicProvider>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
