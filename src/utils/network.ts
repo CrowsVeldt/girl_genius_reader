b317d6540f9ac304d4437d1c0b9d75734b66a6fa
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-19 11:11:57 -0600

    feat: add update to network.ts

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-// TODO: host datefile in some network location
-// TODO: have the datefile update each mon,wed,fri (or whenever the comic updates)
-// TODO: implement function to fetch datefile startup
-// export const fetchDates: () => Promise<string[] | undefined> = async () => {};
-
 export const getData = async () => {
   const data = await axios.get("https://data-collector-yuw1.onrender.com/");
   return data;
@@ -13,7 +8,13 @@ export const getData = async () => {
 export const checkForNewData = async (date: string) => {
   const link = `https://data-collector-yuw1.onrender.com/check?date=${date}`;
   const res = await axios.get(link);
-  console.log(res.data);
-
   return res.data;
 };
+
+export const update = async (date: string) => {
+  const check = await checkForNewData(date)
+  if (check.data) {
+    const newData = await getData()
+  }
+
+}
