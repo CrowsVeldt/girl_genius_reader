0a02f2b9214ff45fdf4efad2bc9a71cc8d543cfd
CrowsVeldt <zechy.mahler@gmail.com>
2024-04-21 20:25:03 -0600

    dev: add type signatures to network funcitons

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,17 +1,21 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-export const getData = async () => {
+export const getData: () => Promise<AxiosResponse> = async () => {
   const data = await axios.get("https://data-collector-yuw1.onrender.com/");
   return data;
 };
 
-export const checkForNewData = async (date: string) => {
+export const checkForNewData: (date: string) => Promise<AxiosResponse> = async (
+  date
+) => {
   const link = `https://data-collector-yuw1.onrender.com/check?date=${date}`;
   const res = await axios.get(link);
   return res.data;
 };
 
-export const update = async (date: string) => {
+export const update: (
+  date: string
+) => Promise<AxiosResponse<any, any> | undefined> = async (date) => {
   try {
     const check = await checkForNewData(date);
     if (check.data) {
