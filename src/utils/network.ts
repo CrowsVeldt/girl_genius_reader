c91324d5a5d398bff955fee19a2b53de3d50f346
CrowsVeldt <zechy.mahler@gmail.com>
2024-05-06 09:56:51 -0600

    dev: add type, comments to network.ts

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,14 +1,17 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosDefaults, AxiosResponse } from "axios";
 
 export const getData: () => Promise<AxiosResponse> = async () => {
   // Fetch data from collector
-  const data: AxiosResponse = await axios.get("https://data-collector-yuw1.onrender.com/");
+  const data: AxiosResponse = await axios.get(
+    "https://data-collector-yuw1.onrender.com/"
+  );
   return data;
 };
 
 export const checkForNewData: (date: string) => Promise<AxiosResponse> = async (
   date
 ) => {
+  // Check for new data
   const link = `https://data-collector-yuw1.onrender.com/check?date=${date}`;
   const res: AxiosResponse = await axios.get(link);
   return res.data;
@@ -18,11 +21,16 @@ export const update: (
   date: string
 ) => Promise<AxiosResponse<any, any> | undefined> = async (date) => {
   try {
+    // Check if there are new dates
     const check: AxiosResponse = await checkForNewData(date);
     if (check.data) {
-      // add type for newData
-      const newData = await getData();
+      // if yes:
+      // fetch data
+      const newData: AxiosResponse = await getData();
+      // and return said data
       return newData;
+    } else {
+      // TODO: render toast that no new data was found
     }
   } catch (error) {
     console.error(error);
