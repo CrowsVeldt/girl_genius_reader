3ceb4d77797de93202a6d468a34f6d0ffb1ee48f
CrowsVeldt <zechy.mahler@gmail.com>
2024-05-11 11:27:04 -0600

    dev: add new data types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -10,3 +10,14 @@ export type PageType = {
   pageNumber: number;
   volumeNumber: number;
 };
+
+export type DateAndTitleType = {
+  date: string;
+  title: string;
+};
+
+export type VolumeType = {
+  volumeStart: string
+  volumeNumber: number
+  pages: PageType[]
+}
