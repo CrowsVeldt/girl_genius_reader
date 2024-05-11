a3acfc618b0f7470a37f90c9c56cabddea5bf77e
CrowsVeldt <zechy.mahler@gmail.com>
2024-05-11 11:31:11 -0600

    dev: add missing types

--- src/listModules/volumes.ts
+++ src/listModules/volumes.ts
@@ -5,7 +5,7 @@ import { DateAndTitleType, PageType, VolumeType } from "../utils/types";
 
 export const collectVolumes: () => void = async () => {
   const root: string = PATH.resolve("./");
-  const pages: any[] = [];
+  const pages: PageType[] = [];
   const parsedTitles: DateAndTitleType[] = await parseTitles();
   const dates: string = await FS.readFile(
     `${root}/lists/dateList.json`,
@@ -13,48 +13,52 @@ export const collectVolumes: () => void = async () => {
   );
   const parsedDates: string[] = JSON.parse(dates);
 
-  const volumeStarts: DateAndTitleType[] = parsedTitles.filter((item) =>
-    item.title.includes("First Page")
+  const volumeStarts: DateAndTitleType[] = parsedTitles.filter(
+    (item: DateAndTitleType) => item.title.includes("First Page")
   );
 
-  const volumeList: VolumeType[] = volumeStarts.map((item, volumeIndex) => {
-    const lastDate: string | null =
-      volumeStarts[volumeIndex + 1] != null
-        ? volumeStarts[volumeIndex + 1].date
-        : null;
+  const volumeList: VolumeType[] = volumeStarts.map(
+    (item: DateAndTitleType, volumeIndex: number) => {
+      const lastDate: string | null =
+        volumeStarts[volumeIndex + 1] != null
+          ? volumeStarts[volumeIndex + 1].date
+          : null;
 
-    const volumeDates: string[] = parsedDates.slice(
-      parsedDates.indexOf(item.date),
-      lastDate != null ? parsedDates.indexOf(lastDate) : parsedDates.length
-    );
-
-    const filteredTitles: DateAndTitleType[] = parsedTitles.filter(
-      (item) => !item.title.includes("First Page")
-    );
-
-    const volumePages: PageType[] = volumeDates.map((date, pageIndex) => {
-      const title: DateAndTitleType | undefined = filteredTitles.find(
-        (item) => item.date === date
+      const volumeDates: string[] = parsedDates.slice(
+        parsedDates.indexOf(item.date),
+        lastDate != null ? parsedDates.indexOf(lastDate) : parsedDates.length
       );
 
-      const page: PageType = {
-        pageNumber: pageIndex + 1,
-        date: date,
-        title: title != null ? title.title : "",
+      const filteredTitles: DateAndTitleType[] = parsedTitles.filter(
+        (item: DateAndTitleType) => !item.title.includes("First Page")
+      );
+
+      const volumePages: PageType[] = volumeDates.map(
+        (date: string, pageIndex: number) => {
+          const title: DateAndTitleType | undefined = filteredTitles.find(
+            (item: DateAndTitleType) => item.date === date
+          );
+
+          const page: PageType = {
+            pageNumber: pageIndex + 1,
+            date: date,
+            title: title != null ? title.title : "",
+            volumeNumber: volumeIndex + 1,
+          };
+
+          pages.push(page);
+
+          return page;
+        }
+      );
+
+      return {
+        volumeStart: item.date,
         volumeNumber: volumeIndex + 1,
+        pages: volumePages,
       };
-
-      pages.push(page);
-
-      return page;
-    });
-
-    return {
-      volumeStart: item.date,
-      volumeNumber: volumeIndex + 1,
-      pages: volumePages,
-    };
-  });
+    }
+  );
 
   FS.writeFile(`${root}/lists/pageList.json`, JSON.stringify(pages));
   FS.writeFile(`${root}/lists/volumeList.json`, JSON.stringify(volumeList));
