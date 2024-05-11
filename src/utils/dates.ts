4bd510376ce21556d8c903ff50c3112ca73e1ea9
CrowsVeldt <zechy.mahler@gmail.com>
2024-05-11 10:57:40 -0600

    dev: add dates.ts, and dates.json

---
+++ src/utils/dates.ts
@@ -0,0 +1,16 @@
+import FS from "node:fs/promises" 
+import PATH from "node:path"
+import dateList from "../../public/dateList.json"
+import { getRss } from "./network"
+
+const fetchDates: () => void = async () => {
+  const root: string = PATH.resolve("./");
+  const dates: string[] = dateList.length > 0 ? dateList : ["20021104"];
+  const rssFeed: any[] = await getRss()
+  const onlyNewDates: string[] = rssFeed.slice(rssFeed.indexOf(dates[dates.length - 1]) + 1)
+  const newList: string[] = [...dates, ...onlyNewDates]
+
+  FS.writeFile(`${root}/lists/dateList.json`, JSON.stringify(newList));
+};
+
+module.exports = { fetchDates };
