d09674f26c5e90193f0d2d7c8ef76142fa4a2a38
CrowsVeldt <zechy.mahler@gmail.com>
2024-05-20 20:09:29 -0600

    dev: add fetchNewDates to network.ts

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -15,3 +15,18 @@ export const getDateList: () => Promise<string[]> = async () => {
   );
   return dateList.data;
 };
+
+export const fetchNewDates: (latestDate: string) => Promise<string[]> = async (
+  latestDate
+) => {
+  try {
+    const rss: any[] = await getRss();
+
+    return rss.slice(rss.indexOf(latestDate) + 1);
+
+  } catch (error) {
+    console.error("Error reading/fetching/writing dates");
+    console.error(error);
+    return [];
+  }
+};
