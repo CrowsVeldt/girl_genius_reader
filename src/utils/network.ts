ecb107beb68565384cab1634ae48765c7913350d
CrowsVeldt <zechy.mahler@gmail.com>
2024-07-04 13:45:17 -0600

    dev: uncomment if statement in update function

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -36,58 +36,56 @@ export const getNextComicDate: (date: string) => Promise<string> = async (
   date
 ) => {
   try {
-    
-  // fetch page markup
-  const { data }: AxiosResponse = await axios.get(
-    `${rootUrl}/comic.php?date=${date}`
-  );
-  // get next date
-  const index: number = data.search("topnext");
-  const nextDate: string = data.slice(index - 14, index - 6);
-  // return next date
-  return nextDate;
+    // fetch page markup
+    const { data }: AxiosResponse = await axios.get(
+      `${rootUrl}/comic.php?date=${date}`
+    );
+    // get next date
+    const index: number = data.search("topnext");
+    const nextDate: string = data.slice(index - 14, index - 6);
+    // return next date
+    return nextDate;
   } catch (error) {
-    
-    console.error(error)
+    console.error(error);
   }
 };
 
 export const update: () => void = async () => {
   try {
     // If new dates are found
-    //if (await areThereNewComics()) {
-    const dateRegex: RegExp = new RegExp(/\d{8}/);
-    // get date list
-    let dateList: string[] = await getDateList();
-    // set current date to start search
-    let currentDate: string = lastElement(dateList);
-    // while current date matches the date regex
-    while (currentDate.match(dateRegex) !== null) {
-      // get next date
-      const nextDate: string = await getNextComicDate(currentDate);
-      // if next date matches the date regex
-      if (nextDate.match(dateRegex) !== null) {
-        // add to date list
-        dateList.push(
-          nextDate === "20030106"
-            ? "20030106b"
-            : nextDate === "20141226"
-            ? "20141226a"
-            : nextDate
-        );
-        // set current date to next date, to progress the loop
-        currentDate = nextDate;
-        // save date list to memory
-        saveData(dateListKey, dateList);
-      } else {
-        // if next date does not match the date regex
-        // save current date to latestSavedDate in memory
-        updateLists();
-        saveData(latestSavedDateKey, currentDate);
-        break;
+    if (await areThereNewComics()) {
+      const dateRegex: RegExp = new RegExp(/\d{8}/);
+      // get date list
+      let dateList: string[] = await getDateList();
+      // set current date to start search
+      let currentDate: string = lastElement(dateList);
+      // while current date matches the date regex
+      while (currentDate.match(dateRegex) !== null) {
+        // get next date
+        const nextDate: string = await getNextComicDate(currentDate);
+        // if next date matches the date regex
+        if (nextDate.match(dateRegex) !== null) {
+          // add to date list
+          dateList.push(
+            nextDate === "20030106"
+              ? "20030106b"
+              : nextDate === "20141226"
+              ? "20141226a"
+              : nextDate
+          );
+          // set current date to next date, to progress the loop
+          currentDate = nextDate;
+          // save date list to memory
+          saveData(dateListKey, dateList);
+        } else {
+          // if next date does not match the date regex
+          // save current date to latestSavedDate in memory
+          updateLists();
+          saveData(latestSavedDateKey, currentDate);
+          break;
+        }
       }
     }
-    //    }
   } catch (error) {
     console.error(error);
   }
