181e606b3891cf7c0835a04332f8852fdb66c53e
Zack <8883408+CrowsVeldt@users.noreply.github.com>
2024-07-13 16:54:12 -0600

    Rework index/volume screen (#34)
    * dev: remove gestureDetector from volumeList
    
    * dev: rework indexScreen to use FlatList to display items
    
    * dev: refactor VolumeList for new ui
    
    * dev: merge from main
    
    * dev: remove volume button from drawer
    
    * feat: get volume number from route params on VolumeScreen
    
    * feat: add get/changeCurrentVolume methods to comicContext
    
    * feat: change volume number from volumeList when pressed
    
    * feat: rework customheader to display correct volume number
    
    * feat: index screen buttons now navigate to given volume's page
    
    * dev: update notes, remove unused import
    
    ---------
    
    Co-authored-by: CrowsVeldt <zechy.mahler@gmail.com>

--- src/components/VolumeList.tsx
+++ src/components/VolumeList.tsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
+import { ContextType, useContext, useState } from "react";
 import {
   Text,
   TouchableOpacity,
   View,
   StyleSheet,
-  ScrollView,
+  ScaledSize,
+  Dimensions,
 } from "react-native";
-import { runOnJS } from "react-native-reanimated";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
-import { TapGesture } from "react-native-gesture-handler/lib/typescript/handlers/gestures/tapGesture";
-import { VolumeType } from "../utils/types";
-import ComicLink from "./ComicLink";
+import { PageType, VolumeType } from "../utils/types";
+import { FlatList } from "react-native-gesture-handler";
+import { ComicContext } from "../context/ComicContext";
+import { formatDate } from "../utils/utilFunctions";
+
+const window: ScaledSize = Dimensions.get("window");
 
 export const VolumeList = ({
   volume,
@@ -19,58 +21,119 @@ export const VolumeList = ({
   volume: VolumeType;
   nav: any;
 }) => {
+  const {
+    changeCurrentPage,
+    changeCurrentVolume,
+  }: ContextType<typeof ComicContext> = useContext(ComicContext);
   const [open, setOpen] = useState<boolean>(false);
 
-  const doubleTap: TapGesture = Gesture.Tap()
-    .maxDuration(250)
-    .numberOfTaps(2)
-    .onStart(() => {
-      runOnJS(setOpen)(false);
-    });
+  const renderElement = (item: PageType, index: number, separators: any) => {
+    return (
+      <TouchableOpacity
+        style={styles.linkButton}
+        onPress={() => {
+          changeCurrentPage(item);
+          nav.navigate("ComicPage");
+        }}
+      >
+        <Text style={styles.linkNumber}>{item.pageNumber}</Text>
+        <Text style={styles.linkText}>{formatDate(item.date)}</Text>
+        <Text style={styles.linkTitle}>{item.title}</Text>
+      </TouchableOpacity>
+    );
+  };
 
   return (
-    <GestureDetector gesture={doubleTap}>
-      <View style={styles.list}>
-        <TouchableOpacity
-          style={styles.title}
-          onPress={() => {
-            setOpen(!open);
-          }}
-        >
-          <Text>{`Volume ${volume.volumeNumber}`}</Text>
-        </TouchableOpacity>
+    <View style={styles.list}>
+      <TouchableOpacity
+        style={styles.pageListToggle}
+        onPress={() => {
+          setOpen(!open);
+        }}
+      >
+        <View style={styles.chevron}></View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.title}
+        onPress={() => {
+          changeCurrentVolume(volume.volumeNumber);
+          nav.navigate("Volume", { volumeNumber: volume.volumeNumber });
+        }}
+      >
+        <Text>{`Volume ${volume.volumeNumber}`}</Text>
         {open && (
-          <View style={styles.subtitleContainer}>
-            <Text style={{ flex: 1 }}>Page #</Text>
-            <Text style={{ flex: 2, marginStart: 20 }}>Date</Text>
-            <Text style={{ flex: 3, paddingStart: 30 }}>Scene</Text>
-          </View>
+          <FlatList
+            data={volume.pages}
+            renderItem={({ item, index, separators }) =>
+              renderElement(item, index, separators)
+            }
+          />
         )}
-        <ScrollView>
-          {volume.pages.map((page, index) =>
-            open ? <ComicLink page={page} nav={nav} key={index} /> : null
-          )}
-        </ScrollView>
-      </View>
-    </GestureDetector>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   list: {
-    minHeight: 30,
-    width: "90%",
-    paddingHorizontal: 10,
-  },
-  title: {
-    alignItems: "center",
-    height: 50,
-    borderBottomWidth: 1,
-    marginBottom: 10,
-  },
-  subtitleContainer: {
+    width: window.width - 20,
     flexDirection: "row",
+    alignItems: "center",
     borderBottomColor: "black",
     borderBottomWidth: 1,
   },
+  title: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+  },
+  subtitleContainer: {
+    flexDirection: "row",
+  },
+  linkButton: {
+    width: "100%",
+    height: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  linkNumber: {
+    marginRight: 6,
+    flex: 1,
+  },
+  linkText: {
+    flex: 2,
+  },
+  linkTitle: {
+    flex: 3,
+    overflow: "visible",
+  },
+  pageListToggle: {
+    height: 50,
+    width: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  chevron: {
+    height: 10,
+    width: 10,
+    borderColor: "black",
+    borderEndWidth: 1,
+    borderBottomWidth: 1,
+  },
 });
+
+// {open && (
+// <View style={styles.subtitleContainer}>
+// <Text style={{ flex: 1 }}>Page #</Text>
+// <Text style={{ flex: 2, marginStart: 20 }}>Date</Text>
+// <Text style={{ flex: 3, paddingStart: 30 }}>Scene</Text>
+// </View>
+// )}
+// <ScrollView>
+// {volume.pages.map((page, index) =>
+// open ? <ComicLink page={page} nav={nav} key={index} /> : null
+// )}
+// </ScrollView>
--- src/screens/IndexScreen.tsx
+++ src/screens/IndexScreen.tsx
@@ -1,24 +1,52 @@
 import { ContextType, useContext } from "react";
-import { StyleSheet, ScrollView } from "react-native";
+import {
+  Dimensions,
+  ScaledSize,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { VolumeType } from "../utils/types";
 import { ComicContext } from "../context/ComicContext";
-import { VolumeList } from "../components/VolumeList";
 import NetStatus from "../components/NetStatus";
+import { FlatList } from "react-native-gesture-handler";
+
+const window: ScaledSize = Dimensions.get("window");
 
 export default function ComicIndex({ navigation }: { navigation: any }) {
-  const { getVolumes }: ContextType<typeof ComicContext> =
+  const { getVolumes, changeCurrentVolume }: ContextType<typeof ComicContext> =
     useContext(ComicContext);
 
   const volumes: VolumeType[] = getVolumes();
 
+  const renderElement = (item: VolumeType, index: number, separators: any) => {
+    return (
+      <TouchableOpacity
+        style={styles.title}
+        onPress={() => {
+          changeCurrentVolume(item.volumeNumber);
+          navigation.navigate("Volume", { volumeNumber: item.volumeNumber });
+        }}
+      >
+        <Text>{`Volume ${item.volumeNumber}`}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <ScrollView contentContainerStyle={styles.list}>
+    <View style={styles.list}>
       <NetStatus />
-      {volumes != null &&
-        volumes.map((volume: VolumeType, index: number) => {
-          return <VolumeList nav={navigation} volume={volume} key={index} />;
-        })}
-    </ScrollView>
+      {volumes != null && (
+        <FlatList
+          data={volumes}
+          contentContainerStyle={styles.list}
+          renderItem={({ item, index, separators }) =>
+            renderElement(item, index, separators)
+          }
+        />
+      )}
+    </View>
   );
 }
 
@@ -26,9 +54,17 @@ const styles = StyleSheet.create({
   list: {
     alignSelf: "center",
     alignItems: "center",
-    width: "100%",
-    paddingTop: 30,
-    paddingBottom: 60,
+    width: window.width,
+    minHeight: "100%",
+    paddingTop: 10,
     backgroundColor: process.env.EXPO_PUBLIC_LIGHT_BG_COLOR,
+  },
+  title: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    width: window.width,
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
