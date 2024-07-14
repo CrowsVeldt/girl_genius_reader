e2dfb792860537bc86e4af913240e7f5b7cbe7b5
CrowsVeldt <zechy.mahler@gmail.com>
2024-07-13 20:51:20 -0600

    feat: update VolumeList for new usage

--- src/components/VolumeList.tsx
+++ src/components/VolumeList.tsx
@@ -21,11 +21,8 @@ export const VolumeList = ({
   volume: VolumeType;
   nav: any;
 }) => {
-  const {
-    changeCurrentPage,
-    changeCurrentVolume,
-  }: ContextType<typeof ComicContext> = useContext(ComicContext);
-  const [open, setOpen] = useState<boolean>(false);
+  const { changeCurrentPage }: ContextType<typeof ComicContext> =
+    useContext(ComicContext);
 
   const renderElement = (item: PageType, index: number, separators: any) => {
     return (
@@ -45,57 +42,36 @@ export const VolumeList = ({
 
   return (
     <View style={styles.list}>
-      <TouchableOpacity
-        style={styles.pageListToggle}
-        onPress={() => {
-          setOpen(!open);
-        }}
-      >
-        <View style={styles.chevron}></View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.title}
-        onPress={() => {
-          changeCurrentVolume(volume.volumeNumber);
-          nav.navigate("Volume", { volumeNumber: volume.volumeNumber });
-        }}
-      >
-        <Text>{`Volume ${volume.volumeNumber}`}</Text>
-        {open && (
-          <FlatList
-            data={volume.pages}
-            renderItem={({ item, index, separators }) =>
-              renderElement(item, index, separators)
-            }
-          />
-        )}
-      </TouchableOpacity>
+      <View style={styles.subtitleContainer}>
+        <Text style={styles.linkNumber}>Page #</Text>
+        <Text style={styles.linkText}>Date</Text>
+        <Text style={styles.linkTitle}>Title</Text>
+      </View>
+      <FlatList
+        data={volume.pages}
+        renderItem={({ item, index, separators }) =>
+          renderElement(item, index, separators)
+        }
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   list: {
-    width: window.width - 20,
-    flexDirection: "row",
-    alignItems: "center",
-    borderBottomColor: "black",
-    borderBottomWidth: 1,
-  },
-  title: {
-    flexGrow: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    height: 50,
+    width: window.width - 50,
+    alignSelf: "center",
   },
   subtitleContainer: {
     flexDirection: "row",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
   },
   linkButton: {
-    width: "100%",
     height: 40,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
   },
   linkNumber: {
     marginRight: 6,
@@ -108,32 +84,4 @@ const styles = StyleSheet.create({
     flex: 3,
     overflow: "visible",
   },
-  pageListToggle: {
-    height: 50,
-    width: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    borderColor: "black",
-    borderWidth: 1,
-  },
-  chevron: {
-    height: 10,
-    width: 10,
-    borderColor: "black",
-    borderEndWidth: 1,
-    borderBottomWidth: 1,
-  },
 });
-
-// {open && (
-// <View style={styles.subtitleContainer}>
-// <Text style={{ flex: 1 }}>Page #</Text>
-// <Text style={{ flex: 2, marginStart: 20 }}>Date</Text>
-// <Text style={{ flex: 3, paddingStart: 30 }}>Scene</Text>
-// </View>
-// )}
-// <ScrollView>
-// {volume.pages.map((page, index) =>
-// open ? <ComicLink page={page} nav={nav} key={index} /> : null
-// )}
-// </ScrollView>
