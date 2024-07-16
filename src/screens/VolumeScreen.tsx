32e566cd37b528f3c75d758ca327f3df6924d4f4
CrowsVeldt <zechy.mahler@gmail.com>
2024-07-16 16:49:27 -0600

    fix: volume scroll now loading all images

--- src/screens/VolumeScreen.tsx
+++ src/screens/VolumeScreen.tsx
@@ -1,21 +1,10 @@
 import { ContextType, useContext, useState } from "react";
-import {
-  Dimensions,
-  Image,
-  ScaledSize,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import {
-  FlatList,
-  Switch,
-  TouchableOpacity,
-} from "react-native-gesture-handler";
+import { Dimensions, ScaledSize, StyleSheet, Text, View } from "react-native";
+import { FlatList, Switch } from "react-native-gesture-handler";
 import { PageType, VolumeType } from "../utils/types";
 import { ComicContext } from "../context/ComicContext";
-import { comicUrl } from "../utils/utilFunctions";
 import { VolumeList } from "../components/VolumeList";
+import VolumeScrollImage from "../components/VolumeScrollImage";
 
 const window: ScaledSize = Dimensions.get("window");
 
@@ -26,23 +15,13 @@ export default function VolumeScreen({
   route: any;
   navigation: any;
 }) {
-  const { getVolume, changeCurrentPage }: ContextType<typeof ComicContext> =
+  const { getVolume }: ContextType<typeof ComicContext> =
     useContext(ComicContext);
+
   const [image, setImage] = useState<boolean>(true);
-  const { volumeNumber } = route.params;
+  const { volumeNumber }: { volumeNumber: number } = route.params;
 
   const volume: VolumeType = getVolume(volumeNumber);
-
-  const renderElement = (item: PageType, index: number) => (
-    <TouchableOpacity
-      onPress={() => {
-        changeCurrentPage(item);
-        navigation.navigate("ComicPage");
-      }}
-    >
-      <Image src={comicUrl(item.date)} style={styles.image} key={index} />
-    </TouchableOpacity>
-  );
 
   return (
     <View style={styles.page}>
@@ -60,14 +39,19 @@ export default function VolumeScreen({
       {image && (
         <FlatList
           data={volume.pages}
-          renderItem={({ item, index }: { item: PageType; index: number }) =>
-            renderElement(item, index)
-          }
+          renderItem={({ item, index }: { item: PageType; index: number }) => (
+            <VolumeScrollImage
+              page={item}
+              navigation={navigation}
+              key={index}
+            />
+          )}
           getItemLayout={(data, index) => ({
             length: window.height - 190,
-            offset: window.height - 190 * index,
+            offset: (window.height - 190) * index,
             index,
           })}
+          initialNumToRender={5}
         />
       )}
       {!image && <VolumeList volume={volume} nav={navigation} />}
@@ -78,10 +62,6 @@ export default function VolumeScreen({
 const styles = StyleSheet.create({
   page: {
     backgroundColor: process.env.EXPO_PUBLIC_LIGHT_BG_COLOR,
-  },
-  image: {
-    height: window.height - 190,
-    width: window.width,
   },
   options: {
     flexDirection: "row",
