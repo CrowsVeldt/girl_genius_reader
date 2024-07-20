31302e011fb87a0a7653210b702bde82728400ca
CrowsVeldt <zechy.mahler@gmail.com>
2024-07-20 16:32:12 -0600

    feat: when currentPage is in currentVolume, upon navigating to VolumeScreen, change index to that of currentPage

--- src/components/VolumeScrollList.tsx
+++ src/components/VolumeScrollList.tsx
@@ -1,48 +1,60 @@
-import React, { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { Dimensions, ScaledSize, StyleSheet } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { PageType } from "../utils/types";
 import VolumeScrollImage from "./VolumeScrollImage";
+import { ComicContext } from "../context/ComicContext";
 
 const window: ScaledSize = Dimensions.get("window");
 
-const VolumeScreenList = React.memo(
-  (props: { pages: PageType[]; navigation: any }) => {
-    const listRef = useRef(null);
+export default function VolumeScreenList(props: {
+  pages: PageType[];
+  navigation: any;
+}) {
+  const { getCurrentPage, getCurrentVolume } = useContext(ComicContext);
+  const listRef = useRef<FlatList>(null);
+  const currentPage = getCurrentPage();
+  const currentVolume = getCurrentVolume();
 
-    useEffect(() => {
-      if (listRef.current) {
+  useEffect(() => {
+    if (listRef.current) {
+      if (currentPage.volumeNumber === currentVolume) {
         listRef.current.scrollToIndex({
-          index: 1,
+          index: currentPage.pageNumber - 1,
+          viewPosition: 0,
+          animated: false,
+        });
+      } else {
+        listRef.current.scrollToIndex({
+          index: 0,
           viewPosition: 0,
           animated: false,
         });
       }
-    }, [listRef]);
+    }
+  }, [currentPage, currentVolume]);
 
-    return (
-      <FlatList
-        ref={listRef}
-        initialScrollIndex={0}
-        contentContainerStyle={styles.list}
-        data={props.pages}
-        renderItem={({ item, index }: { item: PageType; index: number }) => (
-          <VolumeScrollImage
-            page={item}
-            navigation={props.navigation}
-            key={index}
-          />
-        )}
-        getItemLayout={(data, index) => ({
-          length: window.height - 200,
-          offset: (window.height - 200) * index,
-          index,
-        })}
-        initialNumToRender={5}
-      />
-    );
-  }
-);
+  return (
+    <FlatList
+      ref={listRef}
+      contentContainerStyle={styles.list}
+      data={props.pages}
+      renderItem={({ item, index }: { item: PageType; index: number }) => (
+        <VolumeScrollImage
+          page={item}
+          navigation={props.navigation}
+          key={index}
+        />
+      )}
+      getItemLayout={(data, index) => ({
+        length: window.height - 200,
+        offset: (window.height - 200) * index,
+        index,
+      })}
+      initialNumToRender={5}
+    />
+  );
+}
 
 const styles = StyleSheet.create({
   list: {
@@ -50,5 +62,3 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
 });
-
-export default VolumeScreenList;
