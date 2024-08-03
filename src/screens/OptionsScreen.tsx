1dcb01f9b8b03c8260d8cc400be70d708d4fb751
CrowsVeldt <zechy.mahler@gmail.com>
2024-08-03 13:50:43 -0600

    dev: add styles to OptionsScreen

--- src/screens/OptionsScreen.tsx
+++ src/screens/OptionsScreen.tsx
@@ -4,6 +4,18 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Options({ navigation }: { navigation: any }) {
   return (
-      <NetStatus />
+    <View style={styles.screen}>
+        <Text>Options</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignSelf: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: process.env.EXPO_PUBLIC_LIGHT_BG_COLOR,
+  },
+});
