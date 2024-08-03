181f2f4b63a78f4ddf29b579332e7b15579161ab
CrowsVeldt <zechy.mahler@gmail.com>
2024-08-03 15:19:12 -0600

    feat: add scrollDirectionSwitch to OptionsScreen

---
+++ src/components/control components/ScrollDirectionSwitch.tsx
@@ -0,0 +1,63 @@
+import { ContextType, useContext, useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { ScrollDirectionType } from "../../utils/types";
+import { ComicContext } from "../../context/ComicContext";
+
+export default function ScrollDirectionSwitch() {
+  const {
+    getScrollDirection,
+    changeScrollDirection,
+  }: ContextType<typeof ComicContext> = useContext(ComicContext);
+  const [currentDirection, setCurrentDirection] = useState<ScrollDirectionType>(
+    getScrollDirection()
+  );
+
+  return (
+    <View style={styles.option}>
+      <Text>Volume Scroll Direction: </Text>
+      <Pressable
+        onPress={() => {
+          changeScrollDirection("vertical");
+          setCurrentDirection("vertical");
+        }}
+        style={
+          currentDirection === "vertical" ? styles.buttonCurrent : styles.button
+        }
+      >
+        <Text>Vertical</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          changeScrollDirection("horizontal");
+          setCurrentDirection("horizontal");
+        }}
+        style={currentDirection === "horizontal" ? styles.buttonCurrent : styles.button}
+      >
+        <Text>Horizontal</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  option: {
+    flexDirection: "row",
+  },
+  button: {
+    flexDirection: "row",
+    justifyContent: "center",
+    height: 30,
+    borderColor: "black",
+    borderWidth: 1,
+    paddingHorizontal: 4,
+  },
+  buttonCurrent: {
+    flexDirection: "row",
+    justifyContent: "center",
+    height: 30,
+    borderColor: "black",
+    borderWidth: 1,
+    paddingHorizontal: 4,
+    backgroundColor: process.env.EXPO_PUBLIC_DARK_BG_COLOR,
+  },
+});
--- src/screens/OptionsScreen.tsx
+++ src/screens/OptionsScreen.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, View } from "react-native";
 import PreloadOption from "../components/control components/PreloadOptionSwitch";
+import ScrollDirectionSwitch from "../components/control components/ScrollDirectionSwitch";
 
 export default function Options({ navigation }: { navigation: any }) {
   return (
     <View style={styles.screen}>
+      <ScrollDirectionSwitch />
       <PreloadOption />
     </View>
   );
