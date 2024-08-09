28b6b1a3cd28901b62e7f7f72e536d0fe9b99a4e
CrowsVeldt <zechy.mahler@gmail.com>
2024-08-08 18:49:42 -0600

    dev: add buttonContainer View to preloadoptionswitch

--- src/components/control components/PreloadOptionSwitch.tsx
+++ src/components/control components/PreloadOptionSwitch.tsx
@@ -15,41 +15,51 @@ export default function PreloadOption() {
   return (
     <View style={styles.option}>
       <Text>Preload images: </Text>
-      <Pressable
-        onPress={() => {
-          changePreloadPolicy("always");
-          setCurrentPolicy("always");
-        }}
-        style={
-          currentPolicy === "always" ? styles.buttonCurrent : styles.button
-        }
-      >
-        <Text>Always</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => {
-          changePreloadPolicy("wifi");
-          setCurrentPolicy("wifi");
-        }}
-        style={currentPolicy === "wifi" ? styles.buttonCurrent : styles.button}
-      >
-        <Text>On WiFi Only</Text>
-      </Pressable>
-      <Pressable
-        onPress={() => {
-          changePreloadPolicy("never");
-          setCurrentPolicy("never");
-        }}
-        style={currentPolicy === "never" ? styles.buttonCurrent : styles.button}
-      >
-        <Text>Never</Text>
-      </Pressable>
+      <View style={styles.buttonContainer}>
+        <Pressable
+          onPress={() => {
+            changePreloadPolicy("always");
+            setCurrentPolicy("always");
+          }}
+          style={
+            currentPolicy === "always" ? styles.buttonCurrent : styles.button
+          }
+        >
+          <Text>Always</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            changePreloadPolicy("wifi");
+            setCurrentPolicy("wifi");
+          }}
+          style={
+            currentPolicy === "wifi" ? styles.buttonCurrent : styles.button
+          }
+        >
+          <Text>On WiFi Only</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            changePreloadPolicy("never");
+            setCurrentPolicy("never");
+          }}
+          style={
+            currentPolicy === "never" ? styles.buttonCurrent : styles.button
+          }
+        >
+          <Text>Never</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   option: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  buttonContainer: {
     flexDirection: "row",
   },
   button: {
