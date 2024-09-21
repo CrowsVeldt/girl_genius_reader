ef83052fa5661bf0f1b0a1e5e854e899e8b80e0b
CrowsVeldt <zechy.mahler@gmail.com>
2024-09-21 09:40:42 -0600

    feat: enable panning only when zoomed in

--- src/components/ExpoImageZoom.tsx
+++ src/components/ExpoImageZoom.tsx
@@ -1,44 +1,62 @@
+import { useEffect, useRef, useState } from "react";
+import { Dimensions, ScaledSize, StyleSheet } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
 import { Zoomable } from "@likashefqet/react-native-image-zoom";
-import { StyleSheet } from "react-native";
 import { Image } from "expo-image";
 import { ImageZoomRef } from "../components/image_zoom_files/types";
-import { useRef } from "react";
+
+const screen: ScaledSize = Dimensions.get("screen");
+const window: ScaledSize = Dimensions.get("window");
 
 export default function ExpoImageZoom(props: any) {
-  const { uri, scale } = props;
-  const ref = useRef()
+  const [panEnabled, setPanEnabled] = useState<boolean>(true);
+  const { uri } = props;
+  const ref = useRef<ImageZoomRef>();
+  const scale = useSharedValue(1);
+
   return (
     <Zoomable
       ref={ref}
       minScale={1}
-      maxScale={5}
+      maxScale={3}
+      scale={scale}
       doubleTapScale={3}
+      minPanPointers={1}
+      isPanEnabled={panEnabled}
+      isSingleTapEnabled
+      isDoubleTapEnabled
       onInteractionStart={() => {
-        console.log("onInteractionStart");
+        console.log("");
       }}
-      onInteractionEnd={() => console.log("onInteractionEnd")}
-      onPanStart={() => console.log("onPanStart")}
-      onPanEnd={() => console.log("onPanEnd")}
-      onPinchStart={() => console.log("onPinchStart")}
-      onPinchEnd={() => console.log("onPinchEnd")}
-      onSingleTap={() => console.log("onSingleTap")}
+      onInteractionEnd={() => console.log("")}
+      onPanStart={(values) => console.log(values)}
+      onPanEnd={(values) => console.log("")}
+      onPinchStart={(values) => console.log("")}
+      onPinchEnd={(values) => setPanEnabled(scale.value !== 1)}
+      onSingleTap={(values) => console.log("")}
       onDoubleTap={(zoomType) => {
-        console.log("onDoubleTap", zoomType);
+        if (zoomType === "ZOOM_IN") {
+          setPanEnabled(true);
+        } else {
+          setPanEnabled(false);
+        }
       }}
       onProgrammaticZoom={(zoomType) => {
-        console.log("onZoom", zoomType);
+        console.log("");
       }}
-      style={styles.image}
       onResetAnimationEnd={(finished, values) => {
-        console.log("onResetAnimationEnd", finished);
+        console.log("onResetAnimationEnd", values);
         console.log("lastScaleValue:", values?.SCALE.lastValue);
       }}
     >
-      <Image style={styles.image} source={{ uri }} contentFit="cover" />
+      <Image style={styles.image} source={uri} />
     </Zoomable>
   );
 }
 
 const styles = StyleSheet.create({
-  image: {},
+  image: {
+    height: window.height - 100,
+    width: window.width,
+  },
 });
--- src/screens/AckScreen.tsx
+++ src/screens/AckScreen.tsx
@@ -1,11 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
 import NetStatus from "../components/NetStatus";
+import ExpoImageZoom from "../components/ExpoImageZoom";
 
 export default function Acknowledgements() {
   return (
     <View style={styles.screen}>
       <NetStatus />
       <Text>Absolutely everything to do with Girl Genius belongs to the Foglios</Text>
+
+      <ExpoImageZoom uri="http://www.girlgeniusonline.com/ggmain/strips/ggmain20240920a.jpg" />
     </View>
   );
 }
