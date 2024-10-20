98f2c17f51fc1591a59d4d81e16666351fcab11a
CrowsVeldt <zechy.mahler@gmail.com>
2024-10-19 18:22:40 -0600

    feat: add styles to changelogscreen

--- src/screens/ChangeLogScreen.tsx
+++ src/screens/ChangeLogScreen.tsx
@@ -1,10 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { changeList } from "../../changelog";
 
 export default function ChangeLogScreen() {
   return (
-    <View>
-    {changeList.map((item, index) => <Text key={index}>{`${item[0]}:  ${item[1]}`}</Text>)}
+    <View style={styles.screen}>
+    {changeList.map((item, index) => <Text key={index} style={styles.changeItem}>{`${item[0]}:  ${item[1]}`}</Text>)}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignSelf: "center",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: process.env.EXPO_PUBLIC_LIGHT_BG_COLOR,
+  },
+  changeItem: {
+    margin: 10
+  }
+});
