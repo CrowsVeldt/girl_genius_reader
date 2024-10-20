1dab906afe5661c8afe5f270d3ecc95c90885324
CrowsVeldt <zechy.mahler@gmail.com>
2024-10-20 06:41:26 -0600

    feat: add changelogmodal skeleton

---
+++ src/components/ChangeLogModal.tsx
@@ -0,0 +1,9 @@
+import { Modal, Text } from "react-native";
+
+export default function ChangeLogModal () { 
+    return (
+        <Modal onRequestClose={() => {}}>
+            <Text>Kello</Text>
+        </Modal>
+    )
+}
