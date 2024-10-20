d404bffe4c8223e8a7048562e6e887444e8e6acc
CrowsVeldt <zechy.mahler@gmail.com>
2024-10-19 18:19:28 -0600

    feat: grab items from changelist to display on change log

--- changelog.js
+++ changelog.js
@@ -1,5 +1,3 @@
 // Maybe change to a json file
 
-export const changeList = {
-  "0.8.3": "fix panning breaking after double tap",
-};
+export const changeList = [["0.8.3", "fix panning breaking after double tap"]];
--- src/screens/ChangeLogScreen.tsx
+++ src/screens/ChangeLogScreen.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
+import { changeList } from "../../changelog";
 
 export default function ChangeLogScreen() {
   return (
     <View>
-      <Text>Changelog</Text>
+    {changeList.map((item, index) => <Text key={index}>{`${item[0]}:  ${item[1]}`}</Text>)}
     </View>
   );
 }
