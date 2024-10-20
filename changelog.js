0fd5bc3f2ca24f0d8491fbdaef2fd972df4ad7ef
CrowsVeldt <zechy.mahler@gmail.com>
2024-10-19 20:05:47 -0600

    feat: improve changelog items

--- changelog.js
+++ changelog.js
@@ -1,3 +1,10 @@
 // Maybe change to a json file
 
-export const changeList = [["0.8.3", "fix panning breaking after double tap"]];
+export const changeList = [
+  [
+    "0.8.4",
+    "Add version number to home screen",
+    "add Change log screen, with links from drawer menu and home screen",
+  ],
+  ["0.8.3", "fix panning breaking after double tap"],
+];
--- src/screens/ChangeLogScreen.tsx
+++ src/screens/ChangeLogScreen.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import { changeList } from "../../changelog";
 
+const changeLogItem = (item: string[], index: number) => {
+  return (
+    <View key={index} style={styles.changeItem}>
+      <Text>{item[0]}</Text>
+      <View>
+        {item.slice(1).map((subItem, index) => <Text key={`item${index}`}>{subItem}</Text>)}
+      </View>
+    </View>
+  );
+};
+
 export default function ChangeLogScreen() {
   return (
     <View style={styles.screen}>
-    {changeList.map((item, index) => <Text key={index} style={styles.changeItem}>{`${item[0]}:  ${item[1]}`}</Text>)}
+      {changeList.map((item, index) => changeLogItem(item, index))}
     </View>
   );
 }
@@ -13,11 +24,13 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     alignSelf: "center",
-    alignItems: "center",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
     width: "100%",
     backgroundColor: process.env.EXPO_PUBLIC_LIGHT_BG_COLOR,
   },
   changeItem: {
-    margin: 10
-  }
+    flex: 1,
+    flexDirection: "row"
+  },
 });
