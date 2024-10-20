cb7ffbd17d5c32f718b3428fb24c0b122175f9d1
CrowsVeldt <zechy.mahler@gmail.com>
2024-10-20 06:42:46 -0600

    dev: update changelog, notes

--- changelog.js
+++ changelog.js
@@ -1,6 +1,6 @@
 // Maybe change to a json file
 
 export const changeList = [
-  ["0.8.4", "Add version number to home screen", "add Change Log"],
-  ["0.8.3", "fix panning breaking after double tap"],
+  ["0.8.4", "Add version number to home screen", "Add Change Log", "Add Change Log popup"],
+  ["0.8.3", "Fix panning breaking after double tap"],
 ];
