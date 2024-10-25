c6527684984b9a05573accb6da94ffe87d36f6bc
CrowsVeldt <zechy.mahler@gmail.com>
2024-10-25 11:12:05 -0600

    dev: build new version

--- changelog.js
+++ changelog.js
@@ -1,7 +1,11 @@
 // Maybe change to a json file
 
 export const changeList = [
-  ["0.8.5", "Replace bookmark button's star with a bookmark icon"],
+  [
+    "0.8.5",
+    "Replace bookmark button's star with a bookmark icon",
+    "Fix page missing in double-spread - Volume 3, Page 18/19",
+  ],
   [
     "0.8.4",
     "Add version number to home screen",
