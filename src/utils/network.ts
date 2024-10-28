cb0e09aea10c25a2db7b1589575f8e8ede8da0c7
CrowsVeldt <zechy.mahler@gmail.com>
2024-10-28 17:57:20 -0600

    feat: clean up update function, it seems to be working properly now

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -33,9 +33,7 @@ export const getLatestDate: () => Promise<string | undefined> = async () => {
   }
 };
 
-export const areThereNewComics: () => Promise<
-  boolean | undefined
-> = async () => {
+export const areThereNewComics: () => Promise<boolean> = async () => {
   try {
     return (
       (await retrieveData(process.env.EXPO_PUBLIC_LATEST_SAVED_DATE_KEY!)) !==
@@ -44,6 +42,7 @@ export const areThereNewComics: () => Promise<
   } catch (error) {
     console.warn("Error in areThereNewComics function");
     console.error(error);
+    return false;
   }
 };
 
@@ -67,97 +66,71 @@ export const getNextComicDate: (
 };
 
 export const update: () => void = async () => {
-  // TODO: Break update up into smaller conditions
-  // Check for saved list, and for network list, and for new comics
-  // Compare saved and networks lists, so if there are any errors or updates they can be added
-  // If new comics are found update (and trigger update for data collector?)
-  console.log("updating");
   try {
-    // If new dates are found
-    const newComics: boolean | undefined = await areThereNewComics();
-    if (newComics != null && newComics) {
-      console.log("new comics found");
-      // get date list
-      const savedDateList: string[] = await retrieveData(
-        process.env.EXPO_PUBLIC_DATE_LIST_KEY!
-      );
-      const networkDateList: string[] = await getDateList();
-      let dateList: string[] =
-        networkDateList != null
-          ? networkDateList
-          : savedDateList != null
-          ? savedDateList
-          : ["20021104"];
-      // set current date to start search
-      let currentDate: string = lastElement(dateList);
-      // set latest date
-      const latestDate = await getLatestDate();
-      // while current date matches the date regex
-      while (stringOfEightNumbers(currentDate) && currentDate !== latestDate) {
-        console.log("while");
-        // get next date
-        const nextDate: string | undefined = await getNextComicDate(
-          currentDate
-        );
-        // if next date matches the date regex
-        if (
-          nextDate != undefined &&
-          stringOfEightNumbers(nextDate) &&
-          nextDate !== latestDate
-        ) {
-          console.log("next date exists");
-          // add to date list
-          // with exceptions handled below
-          if (nextDate === "20030106") {
-            // 20030106>20030106b (20030106 points to the wrong comic)
-            dateList.push("20030106b");
-          } else if (nextDate === "20040209") {
-            // 20040209-20040209b (Double spread on same webpage)
-            // https://www.girlgeniusonline.com/ggmain/strips/ggmain20040209.jpg
-            // https://www.girlgeniusonline.com/ggmain/strips/ggmain20040209b.jpg
-            dateList.push("20040209");
-            dateList.push("20040209b");
-          } else if (nextDate === "20060425") {
-            // 20060425b-20060425c (Authors added second page)
-            dateList.push("20060425b");
-            dateList.push("20060425c");
-          } else if (nextDate === "20100224") {
-            dateList.push("20100224a");
-          } else if (nextDate === "20100226") {
-            dateList.push("20100226a");
-          } else if (nextDate === "20100301") {
-            dateList.push("20100301a");
-          } else if (nextDate === "20141226") {
-            // 20141226a-20141226b (a is the comic, b is a wallpaper)
-            dateList.push("20141226a");
-            dateList.push("20141226b");
-          } else if (nextDate === "20240211") {
-            // no comic for this date
-            console.log("Nothing to add");
-          } else if (nextDate === "20241023") {
-            dateList.push("20241023a");
-            // "b" is a double-width image
-            // dateList.push("20241023b")
-          } else {
-            dateList.push(nextDate);
-          }
+    const savedDateList: string[] = await retrieveData(
+      process.env.EXPO_PUBLIC_DATE_LIST_KEY!
+    );
+    const networkDateList: string[] = await getDateList();
 
-          // set current date to next date, to progress the loop
-          currentDate = nextDate;
-          console.log("looping");
-          // save date list to memory
-          saveData(process.env.EXPO_PUBLIC_DATE_LIST_KEY!, dateList);
+    const dateList =
+      savedDateList != null &&
+      savedDateList.length > 0 &&
+      savedDateList.length > networkDateList.length
+        ? savedDateList
+        : networkDateList;
+
+    // set current date to start search
+    let currentDate: string = lastElement(dateList);
+    // set latest date
+    const latestDate = await getLatestDate();
+    // while current date matches the date regex
+    while (stringOfEightNumbers(currentDate) && currentDate !== latestDate) {
+      // get next date
+      const nextDate: string | undefined = await getNextComicDate(currentDate);
+      // if next date matches the date regex
+      if (nextDate != undefined && stringOfEightNumbers(nextDate)) {
+        // with exceptions handled below
+        if (nextDate === "20030106") {
+          // 20030106>20030106b (20030106 points to the wrong comic)
+          dateList.push("20030106b");
+        } else if (nextDate === "20040209") {
+          // 20040209-20040209b (Double spread on same webpage)
+          // https://www.girlgeniusonline.com/ggmain/strips/ggmain20040209.jpg
+          // https://www.girlgeniusonline.com/ggmain/strips/ggmain20040209b.jpg
+          dateList.push("20040209");
+          dateList.push("20040209b");
+        } else if (nextDate === "20060425") {
+          // 20060425b-20060425c (Authors added second page)
+          dateList.push("20060425b");
+          dateList.push("20060425c");
+        } else if (nextDate === "20100224") {
+          dateList.push("20100224a");
+        } else if (nextDate === "20100226") {
+          dateList.push("20100226a");
+        } else if (nextDate === "20100301") {
+          dateList.push("20100301a");
+        } else if (nextDate === "20141226") {
+          // 20141226a-20141226b (a is the comic, b is a wallpaper)
+          dateList.push("20141226a");
+          dateList.push("20141226b");
+        } else if (nextDate === "20240211") {
+          // no comic for this date
+          console.log("Nothing to add");
+        } else if (nextDate === "20241023") {
+          dateList.push("20241023a");
+          // "b" is a double-width image
+          // dateList.push("20241023b")
         } else {
-          console.log("else");
-          // if next date does not match the date regex
-          // save current date to latestSavedDate in memory
-          updateLists();
-          saveData(process.env.EXPO_PUBLIC_LATEST_SAVED_DATE_KEY!, currentDate);
-          currentDate = latestDate!;
+          dateList.push(nextDate);
         }
-      }
-    }
-    return true;
+
+        // set current date to next date, to progress the loop
+        currentDate = nextDate;
+        // save date list to memory
+        saveData(process.env.EXPO_PUBLIC_DATE_LIST_KEY!, dateList);
+      }     }
+    updateLists();
+    saveData(process.env.EXPO_PUBLIC_LATEST_SAVED_DATE_KEY!, currentDate);
   } catch (error) {
     console.warn("Error in update function");
     console.error(error);
