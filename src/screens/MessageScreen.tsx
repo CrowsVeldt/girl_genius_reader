5911ee6af19c92db2f9fa706d62b34142b449b10
CrowsVeldt <zechy.mahler@gmail.com>
2024-11-13 06:43:07 -0700

    feat: add messageScreen form

--- src/screens/MessageScreen.tsx
+++ src/screens/MessageScreen.tsx
@@ -1,11 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import NetStatus from "../components/NetStatus";
+import { useState } from "react";
+
+const sendMail = () => {};
 
 export default function MessageScreen() {
+  const [selectedTopic, setSelectedTopic] = useState<string>("");
+  const [text, setText] = useState<string>("Write your message here");
   return (
     <View style={styles.screen}>
       <NetStatus />
-      <Text>Message Screen</Text>
+      <Text>Send a message to the developer</Text>
+      <Text>Subject:</Text>
+      <Picker
+        selectedValue={selectedTopic}
+        onValueChange={(value, index) => setSelectedTopic(value)}
+      >
+        <Picker.Item label="Bug Report" value={"bug"} />
+        <Picker.Item label="Feature Request" value={"feature"} />
+        <Picker.Item label="Other" value={"other"} />
+      </Picker>
+      <Text>Message:</Text>
+      <TextInput
+        editable
+        multiline
+        value={text}
+        onChangeText={(input) => setText(input)}
+      />
+      <Button title="Send" onPress={() => sendMail()} />
     </View>
   );
 }
