2f0ae4fe44abdc36991dd1733b5ddf6ee960ba63
CrowsVeldt <zechy.mahler@gmail.com>
2024-11-19 19:24:14 -0700

    dev: disable swipe to open drawer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,39 +13,39 @@ export default function Layout() {
           <Drawer>
             <Drawer.Screen
             name="index"
-            options={{drawerLabel: "Home", title: "Home"}}
+            options={{drawerLabel: "Home", title: "Home", swipeEnabled: false}}
             />
             <Drawer.Screen
             name="comicpage"
-            options={{drawerLabel: "Comic Page", title: "Comic"}}
+            options={{drawerLabel: "Comic Page", title: "Comic", swipeEnabled: false}}
             />
             <Drawer.Screen
             name="bookmarks"
-            options={{drawerLabel: "Bookmarks", title: "Bookmarks"}}
+            options={{drawerLabel: "Bookmarks", title: "Bookmarks", swipeEnabled: false}}
             />
             <Drawer.Screen
             name="volume"
-            options={{drawerLabel: "Volume", title: "Volume"}}
+            options={{drawerLabel: "Volume", title: "Volume", swipeEnabled: false}}
             />
             <Drawer.Screen
             name="options"
-            options={{drawerLabel: "Options", title: "Options"}}
+            options={{drawerLabel: "Options", title: "Options", swipeEnabled: false}}
             />
             <Drawer.Screen
             name="privacy"
-            options={{drawerLabel: "Privacy", title: "Privacy"}}
+            options={{drawerLabel: "Privacy", title: "Privacy", swipeEnabled: false}}
             />
             <Drawer.Screen
             name="acknowledgments"
-            options={{drawerLabel: "Acknowledgments", title: "Acknowledgments"}}
+            options={{drawerLabel: "Acknowledgments", title: "Acknowledgments", swipeEnabled: false}}
             />
             <Drawer.Screen
             name="changelog"
-            options={{drawerLabel: "Changelog", title: "Changelog"}}
+            options={{drawerLabel: "Changelog", title: "Changelog", swipeEnabled: false}}
             />
             <Drawer.Screen
             name="message"
-            options={{drawerLabel: "Message", title: "Message"}}
+            options={{drawerLabel: "Message", title: "Message", swipeEnabled: false}}
             />
           </Drawer>
         </ComicProvider>
