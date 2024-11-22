60cbc6ba24c07762c9dbdbd2aa85f67769457d7f
CrowsVeldt <zechy.mahler@gmail.com>
2024-11-21 18:14:20 -0700

    dev: remove list of date exceptions from updateDateList function

--- utils/network.ts
+++ utils/network.ts
@@ -77,47 +77,8 @@ export const updateDateList: () => Promise<string[] | undefined> = async () => {
       const nextDate: string | undefined = await getNextComicDate(currentDate);
       // if next date matches the date regex
       if (nextDate != undefined && stringOfEightNumbers(nextDate)) {
-        // with exceptions handled below
-        if (nextDate === "20030106") {
-          // 20030106>20030106b (20030106 points to the wrong comic)
-          dateList.push("20030106b");
-        } else if (nextDate === "20041001") {
-          dateList.push("20041001");
-          dateList.push("20041001a");
-        } else if (nextDate === "20040209") {
-          // 20040209-20040209b (Double spread on same webpage)
-          // https://www.girlgeniusonline.com/ggmain/strips/ggmain20040209.jpg
-          // https://www.girlgeniusonline.com/ggmain/strips/ggmain20040209b.jpg
-          dateList.push("20040209");
-          dateList.push("20040209b");
-        } else if (nextDate === "20060425") {
-          // 20060425b-20060425c (Authors added second page)
-          dateList.push("20060425b");
-          dateList.push("20060425c");
-        } else if (nextDate === "20100224") {
-          dateList.push("20100224a");
-        } else if (nextDate === "20100226") {
-          dateList.push("20100226a");
-        } else if (nextDate === "20100301") {
-          dateList.push("20100301a");
-        } else if (nextDate === "20141226") {
-          // 20141226a-20141226b (a is the comic, b is a wallpaper)
-          dateList.push("20141226a");
-          dateList.push("20141226b");
-        } else if (nextDate === "20151127") {
-          dateList.push("20151127a");
-          dateList.push("20151127b");
-        } else if (nextDate === "20240211") {
-          // no comic for this date
-          console.log("Nothing to add");
-        } else if (nextDate === "20241023") {
-          dateList.push("20241023a");
-          // "b" is a double-width image
-          // dateList.push("20241023b")
-        } else {
-          dateList.push(nextDate);
-        }
-
+        // push the date to dateList
+        dateList.push(nextDate);
         // set current date to next date, to progress the loop
         currentDate = nextDate;
         // save date list to memory
